c1649de2996bd64139fcc73dac3d7ef58446e701
Tony Zelencov <tony.zelencov@codwer.com>
2025-08-22 20:14:24 +0300

    .net version change

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,8 +3,12 @@ using Mood.API.Models;
 
 namespace Mood.API.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         public DbSet<User> Users => Set<User>();
         public DbSet<Couple> Couples => Set<Couple>();
         public DbSet<Nudge> Nudges => Set<Nudge>();
